23140a109152c94a7400521cc89aea5ef015d4dd
Kasper Svane <svane.kasper20@gmail.com>
2021-03-07 15:47:58 +0100

    Todo List project

---
+++ Frontend Frameworks/React/Todo List/src/containers/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import TodoList from '../components/TodoList';
+import './App.css';
+
+function App() {
+    return (
+        <div className='todo-app'>
+            <TodoList />
+        </div>
+    );
+}
+
+export default App;
